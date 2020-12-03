7d57a30ef3d38da2f6799a699cd2a851bc11b3b6
Thomas Rager <tomrager@yahoo.com>
2020-12-03 15:08:27 -0500

    set up burger db

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    burger_name varchar(30),
+    devoured boolean
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Banzai',1);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Bleu Royale',1);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Shrimp',0);
