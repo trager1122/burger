bc821cff70e521ef893194d1a9c33614db897ae7
Thomas Rager <tomrager@yahoo.com>
2020-12-08 12:19:49 -0500

    Updated orm, burger, controller js files

--- config/orm.js
+++ config/orm.js
@@ -1,21 +1,83 @@
 const connection=require('connection.js');
 
+// Helper function for SQL syntax.
+// Allows us to pass 3 values into the mySQL query.
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+  
+  // Helper function to convert object key/value pairs to SQL syntax
+  function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+      var value = ob[key];
+      if (Object.hasOwnProperty.call(ob, key)) {
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+    return arr.toString();
+  }
+
 var orm={
-
-    selectAll{
-
+    //method for displaying all burgers from db
+    selectAll:(tableInput, cb)=>{
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
     },
 
-    insertOne{
+    //method to insert a new burger into the db
+    insertOne:(table, cols, vals, cb)=>{
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },   
 
-    },
+    //method to update the burger's information especially on whether it has been devoured
+    updateOne:(table, objColVals, condition, cb)=>{
+        var queryString ="UPDATE"+ table;
 
-    updateOne{
-
-    },
-
-
-
+        querystring+=" SET "
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
+        });
+    }
 }
 
 module.exports=orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,47 @@
-const express=require('express');
-const burger=require('../models/burger');
+var express = require("express");
 
-//Create router
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var cat = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  cat.selectAll(function(data) {
+    var hbsObject = {
+      burger: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  cat.insertOne(["burger_name", "devoured"], [req.body.name, req.body.sleepy], function(result) {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  cat.updateOne(
+    {
+      devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+
+    }
+  );
+});
+
+// Export routes for server.js to use.
+module.exports = router;
